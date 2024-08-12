891b8448ace80dcae67fcf6fa5c78065ccd50cad
thanh <75254418+th50989@users.noreply.github.com>
2024-08-12 10:48:01 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/api/data', (req, res) => {
+    console.log(req.params);
+    console.log(req.body);
+    console.log(req.baseUrl);
+
+
+  const mockData = {
+    id: 1,
+    name: "John Doe",
+    email: "johndoe@example.com",
+    age: 25,
+    occupation: "Software Developer"
+  };
+
+  res.json(mockData);
+  console.log("has request");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
